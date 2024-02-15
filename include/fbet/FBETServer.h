b3ba0fa91a075f716247c6b91c7652978a737f70
ozerbar <ozerbar@hotmail.com>
2024-02-15 20:02:13 +0100

    working on ros noetic ubuntu_20_04

--- include/fbet/FBETServer.h
+++ include/fbet/FBETServer.h
@@ -28,7 +28,8 @@
 #include <octomap/OcTreeKey.h>
 #include <octomap/octomap.h>
 #include <octomap/OcTree.h>
-#include <octomap/OcTreeLUT.h>
+// #include <octomap/OcTreeLUT.h>
+#include <fbet/OctomapLUT.h>
 
 #include <vector>
 #include <stdio.h>
@@ -123,7 +124,7 @@ namespace fbet_server
 
 	KeySet frontier_cells;
        KeySet candidate_cells;
-	octomap::OcTreeLUT lut;
+	octomap_lut::OcTreeLUT lut;
 	ofstream logfile;
 	point3d best_frontiergoal;
     };
